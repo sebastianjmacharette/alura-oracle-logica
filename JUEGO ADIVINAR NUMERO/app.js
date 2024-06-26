ef5f20da76a88ae22cced84c3fe69ebb99856eb6
sebastianjmacharette <dev@macharette.com.ar>
2024-06-26 12:15:35 -0300

    subida 26/06/2024

--- JUEGO ADIVINAR NUMERO/app.js
+++ JUEGO ADIVINAR NUMERO/app.js
@@ -1,9 +1,42 @@
 let numeroSecreto = 0;
 let intentos = 0;
+let listaNumerosSorteados = [];
+let numerosMaximo = 10;
 //funcion
 
 function generarNumeroSecreto() {
-  return Math.floor(Math.random() * 10) + 1;
+  let numeroGenerado = Math.floor(Math.random() * numerosMaximo) + 1;
+  console.log(numeroGenerado);
+  console.log(listaNumerosSorteados);
+  //si ya sorteamos todos los numeros para que no entre en un bucle finalizamos el juego
+if (listaNumerosSorteados.length == numerosMaximo) {
+ 
+   //informamos que el juego finalizado
+  asignarTextoElemento("h1", "Juego finalizado");
+  asignarTextoElemento(
+    "p",
+    `Felicidades, adivinaste todos numeros
+     `
+  );
+  document.getElementById("reiniciar").removeAttribute("disabled");
+  return; //buena practica aunque no retorne nada
+  
+} else {
+
+
+
+  //tenemos que preguntarnos si el numero esta en la lista, y dependiendo de si esta o no realizar una accion 
+  if (listaNumerosSorteados.includes(numeroGenerado)) {
+    //utilizar concepto de recursividad, que la misma funcion se llame a si misma
+    return generarNumeroSecreto();
+
+    
+  } else {
+    //si el numero no esta , entonces devuelve el numero generado y lo guarda en la lista
+    listaNumerosSorteados.push(numeroGenerado);
+    return numeroGenerado;
+  }
+}
 }
 function asignarTextoElemento(elemento, texto) {
   let elementoHTML = document.querySelector(elemento);
@@ -47,7 +80,7 @@ function condicionesIniciales() {
   //   limpirar caja
   limpiarCaja();
   asignarTextoElemento("h1", "juego del numero secreto");
-  asignarTextoElemento("p", "indica un numero del 1 al 10");
+  asignarTextoElemento("p", `indica un numero del 1 al ${numerosMaximo}`);
   //generar numero aleatorio
   numeroSecreto = generarNumeroSecreto();
   //inicializar el numero ed intentos
